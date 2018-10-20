af54372f0deef8b6cba2f2cc2a305dc83ee9904e
hamger <2313303800@qq.com>
2018-10-20 22:20:38 +0800

    接入数据库

--- readpdf.py
+++ readpdf.py
@@ -9,7 +9,6 @@ import logging
 logger = logging.getLogger()
 # 设置控制台只打印错误
 logger.setLevel(logging.ERROR)
-
 from pdfminer.pdfparser import PDFParser, PDFDocument
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.converter import PDFPageAggregator
@@ -17,6 +16,7 @@ from pdfminer.layout import LTTextBoxHorizontal, LAParams
 from pdfminer.pdfinterp import PDFTextExtractionNotAllowed
 import re
 import json
+import pymysql
 
 # 你需要在当前文件的目录下运行改文件
 path = './static/Tuesdays_with_Morrie.pdf'
@@ -54,12 +54,11 @@ def parse():
 
         # 循环遍历列表，每次处理一个page的内容
         for index, page in enumerate(doc.get_pages()):  # doc.get_pages() 获取page列表
-            if index < 3:
-                continue
-            if index == 4:
-                break
+            # if index < 3:
+            #     continue
+            # if index == 4:
+            #     break
 
-            # print('page_' + str(index + 1))
             # 使用页面解释器来读取
             interpreter.process_page(page)
             # 使用聚合器获取内容
@@ -68,21 +67,38 @@ def parse():
             for out in layout:
                 if hasattr(out, "get_text"):
                     # 去除特殊内容 's 'm 're n't
-                    text = re.sub(r'(\'s|\'m|\'re|n\'t)', ' ', out.get_text())
+                    text = re.sub(r'(\d|\'s|\'m|\'re|n\'t)', '', out.get_text())
                     # 去除标点符号，且将多个空格转化为一个空格
                     text2 = re.sub(r'[\s+\?\.\!\/_,$%^*(+\"\']+|[+——！，。？、~@#￥%……&*（）：]+', ' ', text)
                     words = text2.split()
                     for word in words:
-                        if word.find('cid:') > -1:
+                        w = word.lower()
+                        if w.find('cid:') > -1:
                             continue
                         amount = amount + 1
-                        if obj.__contains__(word):
-                            obj[word] = obj[word] + 1
+                        if obj.__contains__(w):
+                            obj[w] = obj[w] + 1
                         else:
-                            obj[word] = 1
+                            obj[w] = 1
+    for key in obj:
+        connection = pymysql.connect(host='localhost',
+                                        user='root',
+                                        password='',
+                                        db='test',
+                                        charset='utf8mb4')
+        try:
+            # 获取会话指针
+            with connection.cursor() as cursor:
+                # 创建一条sql语句
+                sql = "REPLACE INTO `words` (`word`, `count`, `probability`) VALUES(%s, %s, %s)"
+                # 执行sql语句
+                cursor.execute(
+                    sql, (key, obj[key], round(obj[key] / amount * 100, 4)))
+                # 提交
+                connection.commit()    
+        finally:
+            connection.close()
     print(amount)
-    print(json.dumps(obj, sort_keys=True, indent=4, separators=(',', ':')))
-
 
 if __name__ == '__main__':
     parse()
