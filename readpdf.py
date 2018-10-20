b852b01f0b13662df69a427b132262e5bf19d354
hamger <2313303800@qq.com>
2018-10-20 18:35:36 +0800

    输出单词的出现次数

--- readpdf.py
+++ readpdf.py
@@ -4,12 +4,19 @@
 import sys
 import importlib
 importlib.reload(sys)
+import logging
+# 获取logger实例
+logger = logging.getLogger()
+# 设置控制台只打印错误
+logger.setLevel(logging.ERROR)
 
 from pdfminer.pdfparser import PDFParser, PDFDocument
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.converter import PDFPageAggregator
 from pdfminer.layout import LTTextBoxHorizontal, LAParams
 from pdfminer.pdfinterp import PDFTextExtractionNotAllowed
+import re
+import json
 
 # 你需要在当前文件的目录下运行改文件
 path = './static/Tuesdays_with_Morrie.pdf'
@@ -28,9 +35,10 @@ def parse():
     doc.set_parser(parser)
 
     # 提供初始化密码
-    # 如果没有密码 就创建一个空的字符串
-    doc.initialize("")
-
+    # 如果没有密码，就创建一个空字符串
+    doc.initialize()
+    obj = {}
+    amount = 0
     # 检测文档是否提供txt转换，不提供就忽略
     if not doc.is_extractable:
         raise PDFTextExtractionNotAllowed
@@ -44,8 +52,14 @@ def parse():
         # 创建PDF解释器
         interpreter = PDFPageInterpreter(resource, device)
 
-        # 循环遍历列表，每次处理一个page的内容 
-        for page in doc.get_pages(): # doc.get_pages() 获取page列表
+        # 循环遍历列表，每次处理一个page的内容
+        for index, page in enumerate(doc.get_pages()):  # doc.get_pages() 获取page列表
+            if index < 3:
+                continue
+            if index == 4:
+                break
+
+            # print('page_' + str(index + 1))
             # 使用页面解释器来读取
             interpreter.process_page(page)
             # 使用聚合器获取内容
@@ -53,7 +67,20 @@ def parse():
 
             for out in layout:
                 if hasattr(out, "get_text"):
-                    print(out.get_text())
+                    text = re.sub(r"[\s+\.\!\/_,$%^*(+\"\']+|[+——！，。？、~@#￥%……&*（）：？]+", " ", out.get_text())
+                    text2 = re.sub(r'\s+', ' ', text)
+                    words = text2.split()
+                    for word in words:
+                        if word.find('cid:') > -1:
+                            continue
+                        amount = amount + 1
+                        if obj.__contains__(word):
+                            obj[word] = obj[word] + 1
+                        else:
+                            obj[word] = 1
+    print(amount)
+    print(json.dumps(obj, sort_keys=True, indent=4, separators=(',', ':')))
+
 
 if __name__ == '__main__':
     parse()
