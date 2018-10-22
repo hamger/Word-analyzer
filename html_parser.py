bda851f5366aae7dedc44fb2173d0b8d75811fa5
hamger <2313303800@qq.com>
2018-10-22 13:59:15 +0800

    fix

--- html_parser.py
+++ html_parser.py
@@ -6,15 +6,30 @@ import re
 
 def _get_data(soup):
     res_data = {}
+    index = 0
+    print('-------')
+    phonetic_node = soup.find_all('span', class_='phonetic')
+    for node in phonetic_node:
+        res_data['phonetic_' + str(index)] = node.get_text()
+        index += 1
 
-    phonetic_node = soup.find('span', class_='phonetic')
-    for index, node in enumerate(phonetic_node):
-        res_data['phonetic_' + index] = node.get_text()
-
+    print(res_data)
+    print('-------2')
     meaning_node = soup.find(
         "div", class_="trans-container").find('ul').find_all('li')
-    for index, node in enumerate(meaning_node):
-        res_data['meaning_' + index] = node.get_text()
+    for node in meaning_node:
+        res_data['meaning_' + str(index)] = node.get_text()
+        index += 1
+
+    print(res_data)
+    # phonetic_node = soup.find('span', class_='phonetic')
+    # for index, node in enumerate(phonetic_node):
+    #     res_data['phonetic_' + index] = node.get_text()
+
+    # meaning_node = soup.find(
+    #     "div", class_="trans-container").find('ul').find_all('li')
+    # for index, node in enumerate(meaning_node):
+    #     res_data['meaning_' + index] = node.get_text()
 
     return res_data
 
